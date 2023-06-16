fb65fadf851f1d0540275b1f96b70c56dc571329
FiocChuck <chasf@google.com>
2023-06-16 09:31:11 -0400

    try catch error

--- src/main.py
+++ src/main.py
@@ -11,36 +11,42 @@ def main(request):
 
     df = import_weather()
 
-    db = connect_tcp_socket()
+    try:
+        db = connect_tcp_socket()
 
-    stmt = sqlalchemy.text(
-        "INSERT INTO open_weather_api (lon,lat,desc_short,desc_long,temp,feels_like,temp_min,temp_max,pressure,humidity,visibility,wind_speed,wind_deg,clouds,dt,sunrise,sunset,timezone,city,processing_time)"
-        "VALUES(:lon,:lat,:desc_short,:desc_long,:temp,:feels_like,:temp_min,:temp_max,:pressure,:humidity,:visibility,:wind_speed,:wind_deg,:clouds,:dt,:sunrise,:sunset,:timezone,:city,:processing_time)")
+        stmt = sqlalchemy.text(
+            "INSERT INTO open_weather_api (lon,lat,desc_short,desc_long,temp,feels_like,temp_min,temp_max,pressure,humidity,visibility,wind_speed,wind_deg,clouds,dt,sunrise,sunset,timezone,city,processing_time)"
+            "VALUES(:lon,:lat,:desc_short,:desc_long,:temp,:feels_like,:temp_min,:temp_max,:pressure,:humidity,:visibility,:wind_speed,:wind_deg,:clouds,:dt,:sunrise,:sunset,:timezone,:city,:processing_time)")
 
-    with db.connect() as conn:
-        conn.execute(stmt,
-                     lon=df['lon'],
-                     lat=df['lat'],
-                     desc_short=df['desc_short'],
-                     desc_long=df['desc_long'],
-                     temp=df['temp'],
-                     feels_like=df['feels_like'],
-                     temp_min=df['temp_min'],
-                     temp_max=df['temp_max'],
-                     pressure=df['pressure'],
-                     humidity=df['humidity'],
-                     visibility=df['visibility'],
-                     wind_speed=df['wind_speed'],
-                     wind_deg=df['wind_deg'],
-                     clouds=df['clouds'],
-                     dt=df['dt'],
-                     sunrise=df['sunrise'],
-                     sunset=df['sunset'],
-                     timezone=df['timezone'],
-                     city=df['city'],
-                     processing_time=df['processing_time']
-                     )
+        with db.connect() as conn:
+            conn.execute(stmt,
+                         lon=df['lon'],
+                         lat=df['lat'],
+                         desc_short=df['desc_short'],
+                         desc_long=df['desc_long'],
+                         temp=df['temp'],
+                         feels_like=df['feels_like'],
+                         temp_min=df['temp_min'],
+                         temp_max=df['temp_max'],
+                         pressure=df['pressure'],
+                         humidity=df['humidity'],
+                         visibility=df['visibility'],
+                         wind_speed=df['wind_speed'],
+                         wind_deg=df['wind_deg'],
+                         clouds=df['clouds'],
+                         dt=df['dt'],
+                         sunrise=df['sunrise'],
+                         sunset=df['sunset'],
+                         timezone=df['timezone'],
+                         city=df['city'],
+                         processing_time=df['processing_time']
+                         )
 
-    # publish(df)
+    except sqlalchemy.SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        print(error)
+        return error
+
+        # publish(df)
 
     return 'finish'
